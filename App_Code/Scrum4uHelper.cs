9dc3b4e07e25acac3df21a26f79d73eef0e2dfb2
s19015 <s19015@edu.wsnhid.pl>
2013-11-24 11:29:05 +0100

    Dodane helper
    Dodalem klasę pomocy, która będzie zawierać różnie nie skategoryzowane
    funkcje

---
+++ App_Code/Scrum4uHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrum4u
+{
+    /// <summary>
+    /// Summary description for Scrum4uHelper
+    /// </summary>
+    public class Scrum4uHelper
+    {
+        public Scrum4uHelper()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
