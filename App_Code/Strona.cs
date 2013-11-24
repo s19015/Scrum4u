5af67c375fa8f99b70f6d6c4c5f67f00bcaf801f
s19015 <s19015@edu.wsnhid.pl>
2013-11-24 11:09:00 +0100

    Dodanie domyślnej klasy
    Dodałem domyślną stronę Strona.cs

---
+++ App_Code/Strona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrum4u
+{
+    /// <summary>
+    /// Summary description for Strona
+    /// </summary>
+    public class Strona : System.Web.UI.Page
+    {
+        public Strona()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
