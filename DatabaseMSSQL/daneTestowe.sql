6ca2b4cc65b472e7b5c33f84a2f041553ab5ad38
BartoszBartniczak <bartoszbartniczak@gmail.com>
2014-04-13 10:32:06 +0200

    Przeniesienie na MSSQL
    Pierwszy backup

--- DatabaseMSSQL/daneTestowe.sql
+++ DatabaseMSSQL/daneTestowe.sql
@@ -2,6 +2,8 @@ begin
 --delete from TokenyRejestracyjne where uzytkownicy_email = 'jtestowy@test.pl';
 --delete from GrupyRobocze where uzytkownicy_email = 'jtestowy@test.pl';
 --delete from Uzytkownicy where uzytkownicy_email = 'jtestowy@test.pl';
+delete from ProjektyZaproszenia where id_zapraszajacego = 'jtestowy@test.pl';
+delete from Projekty where id_menager_projektu = 'jtestowy@test.pl';
 
 --exec rejestracjaUzytkownika @email_uzytkownika = '     jtestowy@test.pl', @imie ='   Jan   ', @nazwisko = '  Testowy  ', @haslo = '20cf0e0caf95a5464ae77ae124829a7a3df03d141d82f532ab75ce6aa17cbe8c';
 --exec aktywacjaKonta @email_uzytkownika = 'jtestowy@test.pl', @token = '27AD8B46828B44CA4F375DA8E9AA64A7';
